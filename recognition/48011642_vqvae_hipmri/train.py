3b7a61cb8c8132cfddf5192094abc57cf41869bd
Jules <debeerjj0@gmail.com>
2025-10-26 20:27:59 +1000

    Refactor training, added testing, added header block, docstrings and comments

--- recognition/48011642_vqvae_hipmri/train.py
+++ recognition/48011642_vqvae_hipmri/train.py
@@ -1,16 +1,23 @@
+"""
+File: train.py
+Author: Jules de Beer
+Last modified: 2025-10-26
+Adapted from https://github.com/MishaLaskin/vqvae
+
+Description: file for training, validating, testing and saving VQVAE
+"""
 import numpy as np
 import torch
 import torch.optim as optim
+import torchvision.transforms.v2 as transforms
 import argparse
 import utils
+import dataset
 from modules import VQVAE
-from ssimindex import calculate_ssim
 
 parser = argparse.ArgumentParser()
 
-"""
-Hyperparameters
-"""
+# Hyper parameters
 timestamp = utils.readable_timestamp()
 
 parser.add_argument("--batch_size", type=int, default=32)
@@ -24,7 +31,6 @@ parser.add_argument("--beta", type=float, default=.25)
 parser.add_argument("--learning_rate", type=float, default=3e-1)
 parser.add_argument("--log_interval", type=int, default=50)
 
-# whether or not to save model
 parser.add_argument("-save", action="store_true")
 parser.add_argument("--filename",  type=str, default=timestamp)
 
@@ -35,84 +41,140 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if args.save:
     print('Results will be saved in ./results/vqvae_' + args.filename + '.pth')
 
-"""
-Load data and define batch data loaders
-"""
+# Define data transforms and load data splits with data loaders from dataset.py
+train_transform = transforms.Compose([
+    transforms.ToImage(),
+    transforms.ToDtype(torch.float32, scale=True),
+    transforms.Resize((128, 64)),
+    transforms.Normalize(mean=[0.28], std=[0.28])
+    ])
 
-training_data, validation_data, training_loader, validation_loader, x_train_var, x_val_var = utils.load_data_and_data_loaders(args.batch_size)
-"""
-Set up VQ-VAE model with components defined in ./models/ folder
-"""
+eval_transform = transforms.Compose([
+    transforms.ToImage(),
+    transforms.ToDtype(torch.float32, scale=True),
+    transforms.Normalize(mean=[0.28], std=[0.28])
+    ])
 
+train_data, train_loader, train_var = dataset.load_data(args.batch_size, '/keras_slices_train', train_transform)
+val_data, val_loader, val_var = dataset.load_data(args.batch_size, '/keras_slices_validate', eval_transform)
+test_data, test_loader, test_var = dataset.load_data(540, '/keras_slices_test', eval_transform)
+
+# Set up VQVAE model with components from modules.py
 model = VQVAE(args.n_hiddens, args.n_residual_hiddens, args.n_residual_layers, args.n_embeddings, args.embedding_dim, args.beta).to(device)
 
-"""
-Set up optimizer and training loop
-"""
-optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, amsgrad=True)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-    optimizer, T_0=5000, eta_min=3e-5
-)
-
-results = {
-    'n_updates': 0,
-    'recon_errors': [],
-    'loss_vals': [],
-    'perplexities': [],
-    'val_loss_vals': [],
-    'val_recon_errors': [],
-    'ssim': []
-}
-
-
 def train():
+    """Train model, including a validation step"""
+
+    # Set up optimiser and leanring rate scheduler
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, amsgrad=True)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+        optimizer, T_0=5000, eta_min=3e-5
+    )
+
+    # Set up dictionary for tracking metrics during training
+    metrics = {
+        'n_updates': 0,
+        'recon_errors': [],
+        'loss_vals': [],
+        'perplexities': [],
+        'val_loss_vals': [],
+        'val_recon_errors': [],
+        'ssim': []
+    }
+
+    # Training loop
     for i in range(args.n_updates):
         model.train()
-        (x, _) = next(iter(training_loader))
+        (x, _) = next(iter(train_loader))
         x = x.to(device)
         optimizer.zero_grad()
 
+        # Calculate training loss
         embedding_loss, x_hat, perplexity = model(x)
-        recon_loss = torch.mean((x_hat - x)**2) / x_train_var
+        recon_loss = torch.mean((x_hat - x)**2) / train_var
         loss = recon_loss + embedding_loss
 
         loss.backward()
         optimizer.step()
+        scheduler.step()
 
-        results["recon_errors"].append(recon_loss.cpu().detach().numpy())
-        results["perplexities"].append(perplexity.cpu().detach().numpy())
-        results["loss_vals"].append(loss.cpu().detach().numpy())
-        results["n_updates"] = i
+        # Save training metrics
+        metrics["recon_errors"].append(recon_loss.cpu().detach().numpy())
+        metrics["perplexities"].append(perplexity.cpu().detach().numpy())
+        metrics["loss_vals"].append(loss.cpu().detach().numpy())
+        metrics["n_updates"] = i
 
+        # Validation
         model.eval()
         with torch.no_grad():
-            (v, _) = next(iter(validation_loader))
+            (v, _) = next(iter(val_loader))
             v = v.to(device)
-            val_embedding_loss, v_hat, _ = model(v)
-            val_recon_loss = torch.mean((v_hat - v)**2) / x_val_var
-            val_loss = val_recon_loss + val_embedding_loss
-            ssim = calculate_ssim(v, v_hat)
-            results["val_recon_errors"].append(val_recon_loss.cpu().detach().numpy())
-            results["val_loss_vals"].append(val_loss.cpu().detach().numpy())
-            results["ssim"].append(ssim)
-        model.train()
 
+            # Calculate validation loss
+            val_embedding_loss, v_hat, _ = model(v)
+            val_recon_loss = torch.mean((v_hat - v)**2) / val_var
+            val_loss = val_recon_loss + val_embedding_loss
+
+            # Calculate structural similarity between original and reconstructed images
+            ssim = utils.calculate_ssim(v, v_hat)
+
+            # Save validation metrics
+            metrics["val_recon_errors"].append(val_recon_loss.cpu().detach().numpy())
+            metrics["val_loss_vals"].append(val_loss.cpu().detach().numpy())
+            metrics["ssim"].append(ssim)
+
+        # Save model and print metrics at regular intervals during training
         if i % args.log_interval == 0:
-            """
-            save model and print values
-            """
+
             if args.save:
                 hyperparameters = args.__dict__
-                utils.save_model_and_results(
-                    model, results, hyperparameters, args.filename)
+                utils.save_model_and_results(model, metrics, hyperparameters, args.filename)
 
-            print('Update #', i, 'Recon Error:',
-                  np.mean(results["recon_errors"][-args.log_interval:]),
-                  'Loss', np.mean(results["loss_vals"][-args.log_interval:]),
-                  'Perplexity:', np.mean(results["perplexities"][-args.log_interval:]),
-                  'SSIM: ', np.mean(results["ssim"][-args.log_interval:]))
-        
-        scheduler.step()
+            print('Update #', i, 
+                  'Recon Loss:', np.mean(metrics["recon_errors"][-args.log_interval:]),
+                  'Loss', np.mean(metrics["loss_vals"][-args.log_interval:]),
+                  'Perplexity:', np.mean(metrics["perplexities"][-args.log_interval:]),
+                  'SSIM: ', np.mean(metrics["ssim"][-args.log_interval:]))
+    
+    utils.plot_metrics(metrics)
+
+def test():
+    """Test model and report structural similarity index accuracy score"""
+    
+    # Set up dictionary for tracking metrics during testing
+    metrics = {
+        'recon_errors': [],
+        'loss_vals': [],
+        'perplexities': [],
+        'ssim': []
+    }
+
+    # Test
+    model.eval()
+    with torch.no_grad():
+        (x, _) = next(iter(test_loader))
+
+        x = x.to(device)
+        embedding_loss, x_hat, perplexity = model(x)
+
+        # Calculate test loss
+        recon_loss = torch.mean((x_hat - x)**2) / test_var
+        loss = recon_loss + embedding_loss
+        # Calculate structural similarity between original and reconstructed images
+        ssim = utils.calculate_ssim(x, x_hat)
+
+        # Save testing metrics
+        metrics["ssim"].append(ssim)            
+        metrics["recon_errors"].append(recon_loss.cpu().detach().numpy())
+        metrics["perplexities"].append(perplexity.cpu().detach().numpy())
+        metrics["loss_vals"].append(loss.cpu().detach().numpy())   
+    
+    # Print overall metrics
+    print('Recon Loss:', np.mean(metrics["recon_errors"]),
+          'Loss', np.mean(metrics["loss_vals"]),
+          'Perplexity:', np.mean(metrics["perplexities"]),
+          'SSIM: ', np.mean(metrics["ssim"]))
 
 if __name__ == "__main__":
     train()
+    test()
