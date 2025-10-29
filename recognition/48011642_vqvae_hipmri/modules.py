Fix spacing around argument equal signs

recognition/48011642_vqvae_hipmri/modules.py
@@ -35,11 +35,11 @@ class ResidualLayer(nn.Module):
             nn.BatchNorm2d(in_dim),
             nn.ReLU(True),
             nn.Dropout2d(0.2),
-            nn.Conv2d(in_dim, res_h_dim, kernel_size = 3, stride = 1, padding = 1, bias = False),
+            nn.Conv2d(in_dim, res_h_dim, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(res_h_dim),
             nn.ReLU(True),
             nn.Dropout2d(0.2),
-            nn.Conv2d(res_h_dim, h_dim, kernel_size = 1, stride = 1, bias = False)
+            nn.Conv2d(res_h_dim, h_dim, kernel_size=1, stride=1, bias=False)
         )
 
     def forward(self, x):
@@ -108,12 +108,12 @@ class VectorQuantizer(nn.Module):
         z_flattened = z.view(-1, self.e_dim)
 
         # distances from z to embeddings e_j, (z - e)^2 = z^2 + e^2 - 2 e * z
-        d = torch.sum(z_flattened**2, dim=1, keepdim = True) + \
-            torch.sum(self.embedding.weight**2, dim = 1) - 2 * \
+        d = torch.sum(z_flattened**2, dim=1, keepdim=True) + \
+            torch.sum(self.embedding.weight**2, dim=1) - 2 * \
             torch.matmul(z_flattened, self.embedding.weight.t())
 
         # find closest encodings
-        min_encoding_indices = torch.argmin(d, dim = 1).unsqueeze(1)
+        min_encoding_indices = torch.argmin(d, dim=1).unsqueeze(1)
         min_encodings = torch.zeros(min_encoding_indices.shape[0], self.n_e).to(device)
         min_encodings.scatter_(1, min_encoding_indices, 1)
 
@@ -152,15 +152,15 @@ class Encoder(nn.Module):
         stride = 2
 
         self.conv_stack = nn.Sequential(
-            nn.Conv2d(in_dim, h_dim // 2, kernel_size = kernel, stride = stride, padding = 1),
+            nn.Conv2d(in_dim, h_dim // 2, kernel_size=kernel, stride=stride, padding=1),
             nn.BatchNorm2d(h_dim // 2),
             nn.ReLU(),
             nn.Dropout2d(0.2),
-            nn.Conv2d(h_dim // 2, h_dim, kernel_size = kernel, stride = stride, padding=1),
+            nn.Conv2d(h_dim // 2, h_dim, kernel_size=kernel, stride=stride, padding=1),
             nn.BatchNorm2d(h_dim),
             nn.ReLU(),
             nn.Dropout2d(0.2),
-            nn.Conv2d(h_dim, h_dim, kernel_size = kernel-1, stride = stride-1, padding = 1),
+            nn.Conv2d(h_dim, h_dim, kernel_size=kernel-1, stride=stride-1, padding=1),
             ResidualStack(h_dim, h_dim, res_h_dim, n_res_layers)
         )
 
@@ -187,15 +187,15 @@ class Decoder(nn.Module):
         stride = 2
 
         self.inverse_conv_stack = nn.Sequential(
-            nn.ConvTranspose2d(in_dim, h_dim, kernel_size = kernel-1, stride = stride-1,
-                               padding = 1),
+            nn.ConvTranspose2d(in_dim, h_dim, kernel_size=kernel-1, stride=stride-1,
+                               padding=1),
             ResidualStack(h_dim, h_dim, res_h_dim, n_res_layers),
-            nn.ConvTranspose2d(h_dim, h_dim // 2, kernel_size = kernel, stride = stride,
-                               padding = 1),
+            nn.ConvTranspose2d(h_dim, h_dim // 2, kernel_size=kernel, stride=stride,
+                               padding=1),
             nn.BatchNorm2d(h_dim // 2),
             nn.ReLU(),
             nn.Dropout2d(0.2),
-            nn.ConvTranspose2d(h_dim//2, 1, kernel_size = kernel, stride = stride, padding = 1)
+            nn.ConvTranspose2d(h_dim//2, 1, kernel_size=kernel, stride=stride, padding=1)
         )
 
     def forward(self, x):
@@ -222,7 +222,7 @@ class VQVAE(nn.Module):
         super(VQVAE, self).__init__()
         # encode image into continuous latent space
         self.encoder = Encoder(1, h_dim, n_res_layers, res_h_dim)
-        self.pre_quantization_conv = nn.Conv2d(h_dim, embedding_dim, kernel_size = 1, stride = 1)
+        self.pre_quantization_conv = nn.Conv2d(h_dim, embedding_dim, kernel_size=1, stride=1)
         # pass continuous latent vector through discretization bottleneck
         self.vector_quantization = VectorQuantizer(n_embeddings, embedding_dim, beta)
         # decode the discrete latent representation

recognition/48011642_vqvae_hipmri/predict.py
@@ -16,12 +16,12 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--model_relative_path", type = str,
-                    default = '/vqvae_data_sun_oct_19_22_46_34_2025.pth')
+parser.add_argument("--model_relative_path", type=str,
+                    default='/vqvae_data_sun_oct_19_22_46_34_2025.pth')
 parser.add_argument("--data_path", type=str,
                     default='/home/groups/comp3710/HipMRI_Study_open/keras_slices_data')
-parser.add_argument("--image_folder", type = str, default = '/keras_slices_test')
-parser.add_argument("--n_predictions", type = int, default = 4)
+parser.add_argument("--image_folder", type=str, default='/keras_slices_test')
+parser.add_argument("--n_predictions", type=int, default=4)
 
 args = parser.parse_args()
 
@@ -53,9 +53,9 @@ utils.display_image_grid(args.n_predictions, x, 'original')
 diffs = []
 labels = []
 for i in range(args.n_predictions):
-    x_t = torch.unsqueeze(x[i], dim = 0)
-    x_r = torch.unsqueeze(x_hat[i], dim = 0)
-    ssim, diff = utils.calculate_ssim(x_t, x_r, full = True)
+    x_t = torch.unsqueeze(x[i], dim=0)
+    x_r = torch.unsqueeze(x_hat[i], dim=0)
+    ssim, diff = utils.calculate_ssim(x_t, x_r, full=True)
     labels.append(round(ssim, 3))
     diffs.append(diff)
 utils.display_image_grid(args.n_predictions, x_hat, 'prediction', labels)

recognition/48011642_vqvae_hipmri/utils.py
@@ -18,7 +18,7 @@ from scipy.signal import savgol_filter
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def calculate_ssim(x, x_hat, full = False):
+def calculate_ssim(x, x_hat, full=False):
     """Calculate structural similarity index for paired image and reconstruction
 
     Args:
@@ -36,11 +36,11 @@ def calculate_ssim(x, x_hat, full = False):
     """
 
     # reshape to (B, H, W) (grayscale image only has one channel anyway)
-    x = torch.squeeze(x, dim = 1)
-    x_hat = torch.squeeze(x_hat, dim = 1)
+    x = torch.squeeze(x, dim=1)
+    x_hat = torch.squeeze(x_hat, dim=1)
 
     x, x_hat = x.cpu().detach().numpy(), x_hat.cpu().detach().numpy()
-    return ssim(x, x_hat, channel_axis = 0, data_range = 255, full = full)
+    return ssim(x, x_hat, channel_axis=0, data_range=255, full=full)
 
 def readable_timestamp():
     """Format timestamp
@@ -94,10 +94,10 @@ def load_model(model_filename):
 
     # Load dictionary
     if torch.cuda.is_available():
-        data = torch.load(path + model_filename, weights_only = False)
+        data = torch.load(path + model_filename, weights_only=False)
     else:
-        data = torch.load(path+model_filename, map_location = lambda storage, loc: storage,
-                          weights_only = False)
+        data = torch.load(path+model_filename, map_location=lambda storage, loc: storage,
+                          weights_only=False)
 
     # Unpack hyperparameters from dictionary
     params = data["hyperparameters"]
@@ -133,8 +133,8 @@ def plot_metrics(metrics):
     # Reconstruction loss (training and validation) over time
     f = plt.figure(figsize = (16,4))
     ax = f.add_subplot(1,4,2)
-    ax.plot(recon_errors, label = 'training')
-    ax.plot(val_recon_errors, label = 'validation')
+    ax.plot(recon_errors, label='training')
+    ax.plot(val_recon_errors, label='validation')
     ax.set_yscale('log')
     ax.set_title('Reconstruction Loss')
     ax.set_xlabel('Iteration')
@@ -147,8 +147,8 @@ def plot_metrics(metrics):
 
     # Loss (training and validation) over time
     ax = f.add_subplot(1,4,1)
-    ax.plot(loss_vals, label = 'training')
-    ax.plot(val_loss, label = 'validation')
+    ax.plot(loss_vals, label='training')
+    ax.plot(val_loss, label='validation')
     ax.set_yscale('log')
     ax.set_title('Overall Loss')
     ax.set_xlabel('Iteration')